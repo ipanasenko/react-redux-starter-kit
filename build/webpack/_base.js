b497f979bd0a3fb7735cbddbdbb6369d8d50816d
Ilya Panasenko <hottdogg2008@gmail.com>
2015-12-17 13:46:01 +0200

    update webpack config to build node module

--- build/webpack/_base.js
+++ build/webpack/_base.js
@@ -1,6 +1,5 @@
 import webpack from 'webpack';
 import cssnano from 'cssnano';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
 import config from '../../config';
 import _debug from 'debug';
 
@@ -8,42 +7,34 @@ const paths = config.utils_paths;
 const debug = _debug('app:webpack:_base');
 debug('Create configuration.');
 
-const CSS_LOADER = !config.compiler_css_modules
-  ? 'css?sourceMap'
-  : [
-    'css?modules',
-    'sourceMap',
-    'importLoaders=1',
-    'localIdentName=[name]__[local]___[hash:base64:5]'
-  ].join('&');
+const CSS_LOADER = !config.compiler_css_modules ? 'css?sourceMap' : [
+  'css?modules',
+  'sourceMap',
+  'importLoaders=1',
+  'localIdentName=[name]__[local]___[hash:base64:5]'
+].join('&');
+
+const CSS_LOADERS = [
+  'style?singleton',
+  CSS_LOADER,
+  'postcss'
+];
 
 const webpackConfig = {
   name: 'client',
-  target: 'web',
-  entry: {
-    app: [
-      paths.base(config.dir_client) + '/app.js'
-    ],
-    vendor: config.compiler_vendor
-  },
+  target: 'node',
+  entry: paths.base(config.dir_client) + '/app.js',
   output: {
-    filename: `[name].[${config.compiler_hash_type}].js`,
+    filename: config.main,
     path: paths.base(config.dir_dist),
-    publicPath: config.compiler_public_path
+    publicPath: config.compiler_public_path,
+    libraryTarget: 'commonjs2'
   },
+  externals: config.compiler_vendor,
   plugins: [
     new webpack.DefinePlugin(config.globals),
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.DedupePlugin(),
-    new HtmlWebpackPlugin({
-      template: paths.client('index.html'),
-      hash: false,
-      filename: 'index.html',
-      inject: 'body',
-      minify: {
-        collapseWhitespace: true
-      }
-    })
+    new webpack.optimize.DedupePlugin()
   ],
   resolve: {
     root: paths.base(config.dir_client),
@@ -84,32 +75,32 @@ const webpackConfig = {
       {
         test: /\.styl/,
         loaders: [
-          'style',
-          CSS_LOADER,
-          'postcss',
+          ...CSS_LOADERS,
           'stylus'
         ]
       },
       {
         test: /\.css$/,
-        loaders: [
-          'style',
-          CSS_LOADER,
-          'postcss'
-        ]
+        loaders: CSS_LOADERS
       },
       /* eslint-disable */
-      { test: /\.woff(\?.*)?$/,  loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff' },
-      { test: /\.woff2(\?.*)?$/, loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff2' },
-      { test: /\.ttf(\?.*)?$/,   loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/octet-stream' },
-      { test: /\.eot(\?.*)?$/,   loader: 'file?prefix=fonts/&name=[path][name].[ext]' },
-      { test: /\.svg(\?.*)?$/,   loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=image/svg+xml' },
-      { test: /\.(png|jpg)$/,    loader: 'url?limit=8192' }
+      {
+        test: /\.woff(\?.*)?$/,
+        loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.woff2(\?.*)?$/,
+        loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff2'
+      },
+      {
+        test: /\.ttf(\?.*)?$/,
+        loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/octet-stream'
+      },
+      { test: /\.eot(\?.*)?$/, loader: 'file?prefix=fonts/&name=[path][name].[ext]' },
+      { test: /\.svg(\?.*)?$/, loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=image/svg+xml' },
+      { test: /\.(png|jpg)$/, loader: 'url?limit=8192' }
       /* eslint-enable */
     ]
-  },
-  sassLoader: {
-    includePaths: paths.client('styles')
   },
   postcss: [
     cssnano({
@@ -128,15 +119,5 @@ const webpackConfig = {
     configFile: paths.base('.eslintrc')
   }
 };
-
-// NOTE: this is a temporary workaround. I don't know how to get Karma
-// to include the vendor bundle that webpack creates, so to get around that
-// we remove the bundle splitting when webpack is used with Karma.
-const commonChunkPlugin = new webpack.optimize.CommonsChunkPlugin({
-  names: ['vendor']
-});
-commonChunkPlugin.__KARMA_IGNORE__ = true;
-
-webpackConfig.plugins.push(commonChunkPlugin);
 
 export default webpackConfig;
--- config/_base.js
+++ config/_base.js
@@ -3,85 +3,90 @@ import _debug from 'debug';
 import path from 'path';
 import { argv } from 'yargs';
 
+const pkg = require('../package.json');
+
 const debug = _debug('app:config:_base');
 const config = {
-  env : process.env.NODE_ENV,
+  env: process.env.NODE_ENV,
+
+  main: pkg.main.split('/').pop(),
+  name: pkg.name,
 
   // ----------------------------------
   // Project Structure
   // ----------------------------------
-  path_base  : path.resolve(__dirname, '../'),
-  dir_client : 'src',
-  dir_dist   : 'dist',
-  dir_server : 'server',
-  dir_test   : 'tests',
+  path_base: path.resolve(__dirname, '../'),
+  dir_client: 'src',
+  dir_dist: 'dist',
+  dir_server: 'server',
+  dir_test: 'tests',
 
   // ----------------------------------
   // Server Configuration
   // ----------------------------------
-  server_host : 'localhost',
-  server_port : process.env.PORT || 3000,
+  server_host: 'localhost',
+  server_port: process.env.PORT || 3000,
 
   // ----------------------------------
   // Compiler Configuration
   // ----------------------------------
-  compiler_css_modules     : true,
-  compiler_enable_hmr      : false,
-  compiler_source_maps     : true,
-  compiler_hash_type       : 'hash',
-  compiler_fail_on_warning : false,
-  compiler_quiet           : false,
-  compiler_public_path     : '/',
-  compiler_stats           : {
-    chunks : false,
-    chunkModules : false,
-    colors : true
+  compiler_css_modules: true,
+  compiler_enable_hmr: false,
+  compiler_source_maps: true,
+  compiler_hash_type: 'hash',
+  compiler_fail_on_warning: false,
+  compiler_quiet: false,
+  compiler_public_path: '/',
+  compiler_stats: {
+    chunks: false,
+    chunkModules: false,
+    colors: true
   },
-  compiler_vendor : [
+  compiler_vendor: [
     'react',
     'react-redux',
+    'react-dom',
     'redux',
-    'redux-actions'
+    'redux-actions',
+    'redux-thunk'
   ],
 
   // ----------------------------------
   // Test Configuration
   // ----------------------------------
-  coverage_enabled   : !argv.watch,
-  coverage_reporters : [
-    { type : 'text-summary' },
-    { type : 'html', dir : 'coverage' }
+  coverage_enabled: !argv.watch,
+  coverage_reporters: [
+    { type: 'text-summary' },
+    { type: 'html', dir: 'coverage' }
   ]
 };
 
 /************************************************
--------------------------------------------------
+ -------------------------------------------------
 
-All Internal Configuration Below
-Edit at Your Own Risk
+ All Internal Configuration Below
+ Edit at Your Own Risk
 
--------------------------------------------------
-************************************************/
+ -------------------------------------------------
+ ************************************************/
 
 // ------------------------------------
 // Environment
 // ------------------------------------
 config.globals = {
-  'process.env'  : {
-    'NODE_ENV' : JSON.stringify(config.env)
+  'process.env': {
+    'NODE_ENV': JSON.stringify(config.env)
   },
-  'NODE_ENV'     : config.env,
-  '__DEV__'      : config.env === 'development',
-  '__PROD__'     : config.env === 'production',
-  '__DEBUG__'    : config.env === 'development' && !argv.no_debug,
-  '__DEBUG_NEW_WINDOW__' : !!argv.nw
+  'NODE_ENV': config.env,
+  '__DEV__': config.env === 'development',
+  '__PROD__': config.env === 'production',
+  '__DEBUG__': config.env === 'development' && !argv.no_debug,
+  '__DEBUG_NEW_WINDOW__': !!argv.nw
 };
 
 // ------------------------------------
 // Validate Vendor Dependencies
 // ------------------------------------
-const pkg = require('../package.json');
-
 config.compiler_vendor = config.compiler_vendor
   .filter(dep => {
     if (pkg.dependencies[dep]) return true;
@@ -103,9 +108,9 @@ config.utils_paths = (() => {
     resolve.apply(resolve, [config.path_base, ...args]);
 
   return {
-    base   : base,
-    client : base.bind(null, config.dir_client),
-    dist   : base.bind(null, config.dir_dist)
+    base: base,
+    client: base.bind(null, config.dir_client),
+    dist: base.bind(null, config.dir_dist)
   };
 })();
 
